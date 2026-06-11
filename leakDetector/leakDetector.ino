d51ffbc796e5394d30d3175f3414fa401a35fa70
mat-benv <mpbenvenutti@gmail.com>
2026-06-11 12:31:25 -0300

    buzzer working

--- leakDetector/leakDetector.ino
+++ leakDetector/leakDetector.ino
@@ -1,21 +1,29 @@
-#define WATER_SENSOR_PIN 25
-#define BUZZER_PIN 33
+#define WATER_SENSOR_PIN 12
+#define BUZZER_PIN 21
 int water_state;
 int prev_water_state;
 
+bool water_sensor(int analog){ //digital was not working, analog value was 1200 or less for water contact, 4095 for not
+  return analog < 2000;
+}
+
 void setup() {
-Serial.begin(9600);
-pinMode(WATER_SENSOR_PIN, INPUT_PULLUP);
-pinMode(BUZZER_PIN, OUTPUT);
-water_state = digitalRead(WATER_SENSOR_PIN);
-Serial.println("Ready.");
+  Serial.begin(9600);
+  pinMode(BUZZER_PIN, OUTPUT);
+  water_state = analogRead(WATER_SENSOR_PIN);
+  Serial.println("Ready.");
 }
 
 void loop() {
   prev_water_state = water_state;
-  water_state = digitalRead(WATER_SENSOR_PIN);
-  if(prev_water_state == HIGH && water_state == LOW){
+  water_state = analogRead(WATER_SENSOR_PIN);
+  delay(500);
+  if(!water_sensor(prev_water_state) && water_sensor(water_state)){
     Serial.println("Water leakage is detected!");
     digitalWrite(BUZZER_PIN, HIGH);
   }
+  else
+  if (water_sensor(prev_water_state) && !water_sensor(water_state)) {
+    Serial.println("No water.");
+    digitalWrite(BUZZER_PIN, LOW);
 }
